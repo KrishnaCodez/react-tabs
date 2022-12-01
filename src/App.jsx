de802b8c0b55ce4b1de3581c093a67ccd90c0445
urvish <urvishpvt@gmail.com>
2022-12-01 19:29:52 +0530

    Add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import ReactTabs from "./components/reactTabs/ReactTabs";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactTabs />
+    </div>
+  );
+}
+
+export default App;
